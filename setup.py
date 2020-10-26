e116ff842c93ba365fbbdc69dfb4e2e750cc326b
MarcosMorales2011 <40769305+LiGhTisTiGhT@users.noreply.github.com>
2020-10-26 17:43:39 -0600

    WORK

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
 
     name="example-pkg-MarcosMorales2011",  # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="Marcos Morales",
     author_email="marcosmorales2011@gmail.com",
     description="A small package for Data Science functions.",
