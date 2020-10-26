eb616503f7dd120bb62854eacb01f9e28fc09c65
MarcosMorales2011 <40769305+LiGhTisTiGhT@users.noreply.github.com>
2020-10-26 12:13:30 -0600

    added

--- code/example_module.py
+++ code/example_module.py
@@ -1,1 +1,9 @@
-example_module 
+"""Lambdata - a collection of Data Science helper functions"""
+
+# Accessing libraries through pipenv
+import pandas as pd
+import numpy as np
+
+list_2 = [12, 52, 2234, 334/2]
+def df_cleaner(df):
+    pass
