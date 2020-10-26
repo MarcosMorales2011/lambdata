73fd18b630f447815074f631573e6a07797b5b21
MarcosMorales2011 <40769305+LiGhTisTiGhT@users.noreply.github.com>
2020-10-26 15:08:53 -0600

    fix

--- code/eda.py
+++ code/eda.py
@@ -5,5 +5,15 @@ import pandas as pd
 import numpy as np
 
 list_2 = [12, 52, 2234, 334/2]
-def df_cleaner(df):
-    pass
+def date_three_col(dr):
+  r = pd.to_datetime(dr)
+  a = pd.DatetimeIndex(dr).year
+  b = pd.DatetimeIndex(dr).month
+  c = pd.DatetimeIndex(dr).day
+  return a, b, c
+
+def all_nulls(dr):
+  return dr.isnull().sum().sum()
+
+def outliers(X):
+  return X[X.between(X.quantile(0.01), X.quantile(0.99))]
